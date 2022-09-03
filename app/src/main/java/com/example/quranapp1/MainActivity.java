d49eec0a8fdd7d9a7fb818da70c9ebd725d1fe8d
bsef19a522 <bsef19a522@pucit.edu.pk>
2022-09-03 23:19:28 +0500

    NavigatorDrawer

--- app/src/main/java/com/example/quranapp1/MainActivity.java
+++ app/src/main/java/com/example/quranapp1/MainActivity.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b;
+    ImageView b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b = findViewById(R.id.button);
+        b = findViewById(R.id.imageView2);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/quranapp1/MainActivity2.java
+++ app/src/main/java/com/example/quranapp1/MainActivity2.java
@@ -1,24 +1,50 @@
 package com.example.quranapp1;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    Button b1, b2;
+    DrawerLayout dl;
+    NavigationView nv;
+    Toolbar tl;
+    ActionBarDrawerToggle ad;
+    Button Surah, Juz;
+    ImageView i;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (ad.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        b1 = findViewById(R.id.button2);
-        b2 = findViewById(R.id.button3);
+        Surah = findViewById(R.id.button3);
+        Juz = findViewById(R.id.button2);
+        i = findViewById(R.id.imageView3);
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        Surah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
@@ -27,12 +53,52 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
 
-        b2.setOnClickListener(new View.OnClickListener() {
+        Juz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity2.this, MainActivity4.class);
                 intent.putExtra("index1","Juzz");
                 startActivity(intent);
+            }
+        });
+
+        dl =findViewById(R.id.drawer_layout);
+        nv =findViewById(R.id.navigationView);
+        ad = new ActionBarDrawerToggle(this, dl, R.string.menu_Open, R.string.close_menu);
+        dl.addDrawerListener(ad);
+        ad.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        Log.i("Menu_Drawer_Tag","Home item is clicked");
+                        dl.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_search:
+                        Log.i("Menu_Drawer_Tag","Search item is clicked");
+                        dl.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_profile:
+                        Log.i("Menu_Drawer_Tag","Profile item is clicked");
+                        dl.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_settings:
+                        Log.i("Menu_Drawer_Tag","Settings item is clicked");
+                        dl.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.nav_share:
+                        Log.i("Menu_Drawer_Tag","Share item is clicked");
+                        dl.closeDrawer(GravityCompat.START);
+                        break;
+                }
+                return true;
             }
         });
     }
